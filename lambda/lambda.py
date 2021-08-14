ccdd6f43c8474522a8ad6ff1b5043e177f15b433
Marek Kuczynski <marek.kuczynski@gmail.com>
2021-08-14 23:01:02 +0200

    Update lambda.py

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -3,15 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from aws_lambda_powertools import Logger, Tracer
 
-modules_to_be_patched = [ "boto3", "requests" ]
+# aws powertools
+modules_to_be_patched = [ "boto3" ]
 tracer = Tracer(patch_modules = modules_to_be_patched)
 
 logger = Logger()
 tracer = Tracer()
 
+# get s3 bucket and setup client
 bucketname = os.environ['s3bucket']
 s3_client = boto3.client('s3')
 
+# convert image to base64
 def get_base64_encoded_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode('utf-8')
@@ -21,13 +24,16 @@ def get_base64_encoded_image(image_path):
 @tracer.capture_lambda_handler
 def handler(event, context):
 
-    url = 'https://' + event['rawPath'][1:]
+    # get url from API input
+    rawurl = event['rawPath'][1:]
+    url = 'https://' + rawurl
     print(url)
 
-    timest = str(int(time.time()))
-    fname = timest + '-screen.png'
+    # set tmp and file paths
+    fname = rawurl.replace('.', '_').replace('/','-') + '-screen.png'
     tmpfile = '/tmp/screen.png'
 
+    # add chromium options
     options = Options()
     options.binary_location = '/usr/lib/chromium-browser/chromium-browser'
    
@@ -37,21 +43,27 @@ def handler(event, context):
     options.add_argument('--single-process')
     options.add_argument('--disable-dev-shm-usage')
 
+    # get start timestamp
     startts = time.time()
+
+    # get url and save screenshot
     driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', chrome_options = options)
+
     driver.get(url)
     driver.save_screenshot(tmpfile)
 
     driver.close()
     driver.quit()
 
+    # get end timestamp
     endts = time.time()
-
     timediff = endts - startts
 
+    # upload screen shot to s3
     s3_client.upload_file(tmpfile, bucketname, fname)
     b64img = get_base64_encoded_image(tmpfile)
     
+    # return HTML response
     response = {
         "statusCode": 200,
         "body": '<html><body>' + url + ' - took ' + str(round(timediff, 2)) + ' seconds <br /><img width = "95%" height = "95%" src = "data:image/png;base64,' + b64img + '" /></body></html>',
