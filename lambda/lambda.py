3955a06ec6bbbb6dd4e88f7086c967c1f4887b52
Marek Kuczynski <marek.kuczynski@gmail.com>
2021-08-14 19:34:56 +0200

    Added S3 signed url, added S3 bucket, added HTTP API Gateway

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,8 +1,14 @@
-import boto3
+import boto3, os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+bucketname = os.environ['s3bucket']
+s3_client = boto3.client('s3')
+
 def handler(event, context):
+
+    print(event)
+
     options = Options()
     options.binary_location = '/usr/lib/chromium-browser/chromium-browser'
 
@@ -10,17 +16,30 @@ def handler(event, context):
     options.add_argument('--no-sandbox')
     options.add_argument('--single-process')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=2560,1600')
 
     driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', chrome_options = options)
-    driver.get('https://marek.rocks/')
+    driver.get(event['url'])
     driver.save_screenshot('/tmp/screen.png')
 
     driver.close()
     driver.quit()
 
+    s3_client.upload_file('/tmp/screen.png', bucketname, 'screenshot.png')
+
+    s3url = s3_client.generate_presigned_url(
+        'get_object',
+        Params = {
+            'Bucket': bucketname,
+            'Key': 'screenshot.png'
+        },
+        ExpiresIn = 3600
+    )
+
     response = {
         "statusCode": 200,
-        "body": "S3 signed URL to screenshot"
+        "body": "S3 signed URL to screenshot " + s3url
     }
 
     return response
--- lib/cdk-screenshot-stack.ts
+++ lib/cdk-screenshot-stack.ts
@@ -1,23 +1,45 @@
 import * as cdk from '@aws-cdk/core';
 import * as path from "path";
-import { DockerImageFunction, DockerImageCode, Function } from "@aws-cdk/aws-lambda";
-import { Runtime, Tracing } from '@aws-cdk/aws-lambda';
-import { CfnOutput, Construct, Duration, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
+import { DockerImageFunction, DockerImageCode } from "@aws-cdk/aws-lambda";
+import { Tracing } from '@aws-cdk/aws-lambda';
+import { CfnOutput, Duration } from '@aws-cdk/core';
 import { Bucket } from '@aws-cdk/aws-s3';
+import { HttpApi } from '@aws-cdk/aws-apigatewayv2';
+import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations';
 
 export class CdkScreenshotStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // create S3 bucket
     const s3bucket = new Bucket(this, 'screenshotbucket');
 
+    // define Docker file for Lambda function
     const dockerfile = path.join(__dirname, "./../lambda");
-
+    
+    // define Lambda function using Docker image
     const lambda = new DockerImageFunction(this, "screenshot", {
       code: DockerImageCode.fromImageAsset(dockerfile),
       memorySize: 2048,
-      timeout: Duration.seconds(60)
+      timeout: Duration.seconds(60),
+      tracing: Tracing.ACTIVE,
+      environment: {
+        "s3bucket": s3bucket.bucketName
+      }
     });
+
+    s3bucket.grantReadWrite(lambda);
+
+    // Create HTTP API Gateway
+    const apigw = new HttpApi(this, 'CdkTypescriptApi', {
+      createDefaultStage: true,
+      defaultIntegration: new LambdaProxyIntegration({
+        handler: lambda
+      })
+    });
+
+    // Print API Gateway URL
+    new CfnOutput(this, 'API URL', { value: apigw.url ?? 'deployment error' });
 
   }
 }
